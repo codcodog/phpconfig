83b18d11b6fcece640dc08a563aaa35580587236
codcodog <codcodog@gmail.com>
2018-11-14 15:32:35 +0000

    调整 code

--- code.php
+++ code.php
@@ -8,16 +8,20 @@
  * API 接口状态码定义
  *
  * 主要定义为以下几种类型：
- * - 非法参数
- * - 数据为空
- * - （业务）操作失败
- * - 权限（文件，登录等）
- * - 成功/失败
+ * 1. 客户端-非法参数
+ * 2. 服务端-数据为空
+ * 3. （业务）操作失败
+ * 4. 权限（文件，登录等）
+ * 5. 成功/失败
  */
 class Code
 {
+    /** 成功&失败 **/
+    const SUCCESS = 0;
+    const FAILED  = 1;
+
     /** 非法参数 1001~1999 **/
-    const PARAM_FORMAT_INVALID = 1001;
+    const PARAM_INVALID = 1001;
 
     /** 数据为空 **/
     const DATA_NOT_FOUND = 2001;
@@ -27,8 +31,4 @@ class Code
 
     /** 权限不足 **/
     const PERMISSION_NOT_ALLOWED = 4001;
-
-    /** 成功/失败 **/
-    const SUCCESS = 0;
-    const FAILED  = 1;
 }
